be9d49d6fa5fd60bbef5a5104855c3993b8de1b7
Kristian K. Andreasen <kristian.kauffeld@gmail.com>
2024-02-06 08:20:43 +0100

    basic poc

--- src/main.py
+++ src/main.py
@@ -1,44 +1,64 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
 import logging
-import aioredis
-from aioredis.exceptions import ResponseError
+import redis
+from rq import Queue
+import requests
+import time
+from rq.job import Job
 
 logging.basicConfig(level=logging.INFO)
 
-redis_url="redis://redis:6379"
+app = FastAPI(debug=True)
 
-# Global variable to store the Redis connection
-redis_db = None
+# Establish a connection to Redis
+r = redis.Redis(host="redis", port=6379, username="default", password="redis", decode_responses=True) #db=0
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    global redis_db
-    try:
-        # using aioredis allow us to interact with Redis without blocking 
-        # the server from handling other requests
-        redis_db = await aioredis.from_url(redis_url, decode_responses=True)
+print(f"Ping successful: {r.ping()}")
 
-        if redis_db:
-            
-            pong = await redis_db.ping()
-            print("Redis ping:", pong)
-        logging.info(f"Connected to Redis")
-    except ResponseError as e:
-        logging.error(f"Failed to connect to Redis: {e}")
-    yield
-    logging.info("Shutting down...")
-    await redis_db.close()
+# Create a queue
+task_queue = Queue(connection=r)
 
-app = FastAPI(lifespan=lifespan, debug=True) 
+def count_words_at_url(url):
+    """Just an example function that's called async."""
+    resp = requests.get(url)
+    return len(resp.text.split())
+
+# Enqueue a job
+job = task_queue.enqueue(count_words_at_url, 'http://nvie.com')
+#task_queue.enqueue(count_words_at_url, 'http://nvie.com')
+
+print('Job id: %s' % job.id)
+print('Status: %s' % job.get_status(refresh=True))
+print(job.return_value)
+#result = Job.fetch(id=job.id, connection=r)
+
+#result.latest_result()  #  returns Result(id=uid, type=SUCCESSFUL)
+
+#if result == result.Type.SUCCESSFUL: 
+#    print(result.return_value) 
+#else: 
+#    print(result.exc_string)
 
 
+#print(job.return_value)
+#result = job.latest_result() 
 
-@app.get("/")
-async def read_root():
-    # Example Redis operation: set a value
-    await redis_db.set('my-key', 'value')
-    
-    # Example Redis operation: get a value
-    value = await redis_db.get('my-key', encoding='utf-8')
-    return {"Hello": "World", "Redis Value": value}
+#print(result.created_at)
+#print(result.type)
+
+#job = Job.fetch(id='my_id', connection=redis)
+#for result in job.results(): 
+#    print(result.created_at, result.type)
+
+#if result == result.Type.SUCCESSFUL: 
+#    print(result.return_value) 
+#else: 
+#    print(result.exc_string)
+
+#if status == "finished":
+#    print(job.return_value)
+
+#redis_url = 'redis://redis:6379'
+
+#redis://[[username]:[password]]@localhost:6379/0
+#r = redis.from_url(url="redis://default:admin@redis:6379/0")
