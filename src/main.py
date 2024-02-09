2351b0fe8a0f75783ab8f24b9f543e28afa07004
Kristian K. Andreasen <kristian.kauffeld@gmail.com>
2024-02-09 08:35:45 +0100

    env variables

--- src/main.py
+++ src/main.py
@@ -6,13 +6,18 @@ from rq import Queue
 
 logging.basicConfig(level=logging.INFO)
 
+REDISHOST = os.getenv("REDISHOST")
+REDISPORT = os.getenv("REDISPORT")
+REDISUSER = os.getenv("REDISUSER")
+REDIS_PASSWORD = os.getenv("REDIS_PASSWORD")
+
 app = FastAPI(debug=True)
 
 #redis://default:redis@redis:6379/0
 
 # Establish a connection to Redis
-redis_db = redis.Redis.from_url(os.environ['REDIS_PRIVATE_URL'])
-#redis_db = redis.Redis(host="redis", port=6379, username="default", password="redis", db=0)
+#redis_db = redis.Redis.from_url(os.environ['REDIS_PRIVATE_URL'])
+redis_db = redis.Redis(host=REDISHOST, port=REDISPORT, username=REDISUSER, password=REDIS_PASSWORD, db=0)
 
 print(f"Ping successful: {redis_db.ping()}")
 
