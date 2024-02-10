cd8074ad12be36797a2705a4d4ed1a302503772f
Kristian K. Andreasen <kristian.kauffeld@gmail.com>
2024-02-10 13:48:58 +0100

    organized FastAPI and RQ Worker into separate services in order to deploy them independently

---
+++ rq-worker/worker.py
@@ -0,0 +1,14 @@
+import os
+
+import redis
+from rq import Worker, Queue, Connection
+
+listen = ['high', 'default', 'low']
+
+REDIS_PRIVATE_URL = os.getenv("REDIS_PRIVATE_URL")
+redis_conn = redis.from_url(REDIS_PRIVATE_URL)
+
+if __name__ == '__main__':
+    with Connection(redis_conn):
+        worker = Worker(map(Queue, listen))
+        worker.work()
